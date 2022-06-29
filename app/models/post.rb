e30c633a60265474e2fddf2aa9215b8bbeceb205
JamesNg <njames15@gmail.com>
2022-06-29 17:57:39 -0400

    commit tree 354f43cd05daebdf56030898f495dc2eddef3356

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    #custom validation
+    validate :check_for_clickbaitability
+
+    
+    def check_for_clickbaitability
+        regex = [/Won't Believe/i, /Secret/i, /Top \d/i, /Guess/i]
+        if regex.none? { |word| word.match title }
+            errors.add(:title, "Title not clickbaity enough!")
+        end
+    end
+
 end
